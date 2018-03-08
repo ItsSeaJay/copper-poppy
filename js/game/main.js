8bbf2375bfd03cd80847f35db3e2eab1076f8de2
Unknown <b6010572@my.shu.ac.uk>
2018-03-08 22:44:33 +0000

    Added game loop functions

--- js/game/main.js
+++ js/game/main.js
@@ -1,7 +1,29 @@
 var game = {
-  title: "Game"
-}
+  init: function () {
 
-$('document').ready(function () {
+  },
+  play: function () {
+    
+  },
+  draw: function () {
+
+  },
+  canvas: {
+    node: $("game")[0],
+    context: $("#game")[0].getContext("2d"),
+    width: $("#game")[0].width,
+    height: $("#game")[0].height,
+    clear: function () {
+      this.context.clearRect(
+        0, // x
+        0, // y
+        this.width,
+        this.height
+      );
+    }
+  }
+};
+
+$("document").ready(function () {
   // TODO: start game loop
 });
